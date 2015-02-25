1f0fb6e54f67d632ddcc0060386f02b841103d76
琼雪 <mengyuan.wmy@alibaba-inc.com>
2015-02-26 02:19:03 +0800

    finish timeline

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,46 @@
-$('#nav').onePageNav({
-    currentClass: 'active'
+$(document).ready(function() {
+    // navbar
+    $('#nav').onePageNav({
+        currentClass: 'active',
+        changeHash: true
+    });
+
+    // tabs
+    $('ul.tabs').each(function(){
+        var $active,
+            $content,
+            $links = $(this).find('a');
+
+        // Show first content as default
+        $active = $($links[0]);
+        $active.addClass('active');
+
+        $content = $($active[0].hash);
+
+        // Hide the remaining content
+        $links.not($active).each(function () {
+            $(this.hash).hide();
+        });
+
+        // Bind the click event handler
+        $(this).on('click', 'a', function(e){
+            // Make the old tab inactive.
+            $active.removeClass('active');
+            $content.hide();
+
+            // Update the variables with the new link and content
+            $active = $(this);
+            $content = $(this.hash);
+
+            // Make the tab active.
+            $active.addClass('active');
+            $content.show();
+
+            // Prevent the anchor's default click action
+            e.preventDefault();
+        });
+    });
+
+
 });
+
